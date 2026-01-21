e5309da0db5c45d7257683d2e1043e14b7ddfa7e
jaegar-wolf <mathiiew972@gmail.com>
2026-01-21 19:00:32 +0100

    update vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue(), vueDevTools()],
+  plugins: [
+    tailwindcss(),
+    vue({
+      // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
+      // For example <img src="/images/logo.png"> will not work without the code below
+      template: {
+        transformAssetUrls: {
+          includeAbsolute: false,
+        },
+      },
+    }),
+    ,
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
