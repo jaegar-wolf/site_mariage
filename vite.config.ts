cde7bd0ad9de26c60fcecdf41079c104e0bd273c
jaegar-wolf <mathiiew972@gmail.com>
2026-01-21 19:02:34 +0100

    update vite conf again

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,6 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     vue({
-      // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
-      // For example <img src="/images/logo.png"> will not work without the code below
       template: {
         transformAssetUrls: {
           includeAbsolute: false,
